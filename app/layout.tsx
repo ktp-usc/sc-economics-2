af8ee6fcfd1e0975abfc0de7ad57962416c47057
AdenHarr <reddragon0828@gmail.com>
2026-03-17 17:13:07 -0400

    Volunteer Application page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
+import Header from "@/components/header";
+
 export const metadata: Metadata = {
-    title: "Volunteer Application | SC Economics",
-    description: "KTP SP26"
+    title: "SC Economics Volunteer",
+    description: "SC Economics Volunteer Management System",
 };
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
         <body>
+        <Header />
         {children}
         </body>
         </html>
